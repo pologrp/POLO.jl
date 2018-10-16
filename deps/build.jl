78f355196719b5d8e4e0bbe9e752ce285a0a5017
Arda Aytekin <aytekin@protonmail.com>
2018-10-16 12:09:45 +0200

    Change the installation folder to user's local directory

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,13 @@
 using BinaryProvider
 
 products = Product[
-  LibraryProduct("/usr/local/lib", "libpoloapi", :libpolo),
+  LibraryProduct(
+    joinpath(expanduser("~"), "usr", "local", "lib"), "libpoloapi", :libpolo
+  ),
 ]
 
 if any(!satisfied(p) for p in products)
-  error("POLO is not installed properly on your system.")
+  error("POLO C-API is not installed properly on your system.")
 end
 
 write_deps_file(joinpath(@__DIR__, "deps.jl"), products)
