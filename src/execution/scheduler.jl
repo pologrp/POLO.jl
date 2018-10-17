6194f9a992a8b0d8a61c00c812fe153eb13eb970
Martin Biel <martinbiel93@gmail.com>
2018-10-17 16:10:59 +0200

    Fixed scheduler signature

--- src/execution/scheduler.jl
+++ src/execution/scheduler.jl
@@ -17,6 +17,28 @@ function initialize!(scheduler::Scheduler, x₀::AbstractVector)
 end
 getx(scheduler::Scheduler) = scheduler.x
 
+function (proxgrad::ProxGradient{Scheduler})(x₀::AbstractVector,termination::AbstractTermination,logger::AbstractLogger)
+    initialize!(proxgrad.execution, x₀)
+    xbegin = pointer(x₀, 1)
+    xend = pointer(x₀, length(x₀) + 1)
+    termination_c = @cfunction(POLO.termination_wrapper, Cint,
+                               (Cint, Cdouble, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Nothing}))
+    log_c = @cfunction(POLO.log_wrapper, Nothing,
+                       (Cint, Cdouble, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Nothing}))
+    return ccall(POLO.run_scheduler, Nothing,
+                 (Ptr{Nothing},Ptr{Cdouble}, Ptr{Cdouble},
+                  Ptr{Nothing}, Any,
+                  Ptr{Nothing}, Any),
+                 proxgrad,
+                 xbegin, xend,
+                 termination_c, termination,
+                 log_c, logger)
+end
+function (proxgrad::ProxGradient{Scheduler})(x₀::AbstractVector)
+    termination = POLO.Utility.MaxIteration(100)
+    return proxgrad(x₀,termination,POLO.Utility.ProgressLogger.Value(termination))
+end
+
 paramserver_handle(::Scheduler) = POLO.proxgradient_scheduler
 execution_handle(::Scheduler) = POLO.run_scheduler
 delete_handle(::Scheduler) = POLO.delete_proxgradient_scheduler
