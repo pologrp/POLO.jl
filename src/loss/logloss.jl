55c02b86deba5bfc5fe4911fc68b8680ab13d3ac
Arda Aytekin <aytekin@protonmail.com>
2018-10-18 17:06:44 +0200

    Further improvements on LogLoss

--- src/loss/logloss.jl
+++ src/loss/logloss.jl
@@ -14,50 +14,16 @@ end
 nsamples(loss::LogLoss)   = size(loss.A, 1)
 nfeatures(loss::LogLoss)  = size(loss.A, 2)
 
-function _value(x::AbstractVector{<:Real}, A::AbstractMatrix, b::AbstractVector)
-  temp = exp.(-b .* (A*x))
-  for idx in 1:length(temp)
-    @inbounds temp[idx] = isinf(temp[idx]) ? -b[idx]*dot(A[idx,:], x) : log1p(temp[idx])
-  end
-  return sum(temp)
-end
-
-value(loss::LogLoss, x::AbstractVector{<:Real}) = _value(x, loss.A, loss.b)
-
-function value(loss::LogLoss, x::AbstractVector{<:Real},
-               comps::AbstractVector{<:Integer})
-  N, d = nsamples(loss), nfeatures(loss)
-  compmin, compmax = extrema(comps)
-  A, b = loss.A[comps,:], loss.b[comps]
-  return _value(x, A, b)
-end
-
-function _gradient!(x::AbstractVector{T}, dx::AbstractVector{T},
-                    A::AbstractMatrix, b::AbstractVector) where {T <: Real}
-  temp = exp.(-b .* (A*x))
-  for idx in 1:length(temp)
-    @inbounds temp[idx] = isinf(temp[idx]) ? -b[idx] : -b[idx]*temp[idx]/(one(T) + temp[idx])
-  end
-  return mul!(dx, transpose(A), temp)
-end
-
-function gradient!(loss::LogLoss, x::AbstractVector{T},
-                   dx::AbstractVector{T}) where {T<:Real}
-  A, b = loss.A, loss.b
-  return _gradient!(x, dx, A, b)
-end
-
-function gradient!(loss::LogLoss, x::AbstractVector{T}, dx::AbstractVector{T},
-                   comps::AbstractVector{<:Integer}) where {T<:Real}
-  N, d = nsamples(loss), nfeatures(loss)
-  compmin, compmax = extrema(comps)
-  A, b = loss.A[comps,:], loss.b[comps]
-  return _gradient!(x, dx, A, b)
-end
-
-# TODO: Still not efficient. Make a helper function to avoid calling `temp =
-# exp.(-b .* (A*x)) twice, i.e., one for `gradient!` and another for `value`.
 function loss!(loss::LogLoss, x::AbstractVector, g::AbstractVector)
-  gradient!(loss,x,g)
-  return value(loss,x)
+  T = eltype(x)
+  val = -loss.b .* (loss.A * x)
+  fval = zero(T)
+
+  for idx in 1:length(val)
+    temp = exp(val[idx])
+    @inbounds fval += isinf(temp) ? val[idx] : log1p(temp)
+    @inbounds val[idx] = isinf(temp) ? -b[idx] : -b[idx]*temp/(one(T) + temp)
+  end
+  mul!(g, transpose(A), temp)
+  return fval
 end
